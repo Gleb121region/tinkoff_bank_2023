2523a9406f254337b38b810f8fe5eda1f925c860
Gleb121region <71015316+Gleb121region@users.noreply.github.com>
2023-03-23 00:24:31 +0300

    Initial commit

---
+++ src/main/java/org/example/First.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+//        int[] arr = {1, 0, 2, 3, 0, 4, 5, 0};
+//        Solution solution = new Solution();
+//        solution.duplicateZeros(arr);
+
+    }
+}
